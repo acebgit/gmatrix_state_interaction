742a3f1c68b46dc63cc190df3c5c5709329033e9
acebreiro <antonio.cebreiro@dipc.org>
2023-03-10 17:17:50 +0100

    All g-tensor files with PyQchem parsers as python path

--- doublets_expansion/g_main.py
+++ doublets_expansion/g_main.py
@@ -27,18 +27,18 @@ from g_plots import get_bar_chart, sos_analysis_and_plot
 # G-TENSOR CALCULATION
 g_calculation = 1
 ras_input = '../\
-RASCI_results/mncn5no_2-_def2tzvp_7_7_d5_sextet_enerproc.out' # str(sys.argv[1])'''
+RASCI_results/cu_mnt2_2-/cu_mnt2_2-_def2tzvp_19_11.out'  # str(sys.argv[1])'''
 
 selected_states = 1 # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
-states_ras = [1,6] # States to be included when "selected_states = 0"
+states_ras = [1,2,3] # States to be included when "selected_states = 0"
 symmetry_selection = 'Ag' # Symmetry selected states
 selected_SOC = 0 # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
 
 # EXCITED STATES ANALYSIS IN ras
-excited_states_analysis = 1
+excited_states_analysis = 0
 new_active_space = 0
 sos_analysis = 0
-bar_plots = 0
+bar_plots = 1
 
 # eom ANALYSIS AND ras-eom ENERGIES EXCHANGE
 eom_information = 0
