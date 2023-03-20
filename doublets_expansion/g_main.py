cbae69630ddab8dc1f787af8d84a8d6ea3ab3f6e
acebreiro <antonio.cebreiro@dipc.org>
2023-03-20 16:56:01 +0100

    Uploading Fe outputs

--- doublets_expansion/g_main.py
+++ doublets_expansion/g_main.py
@@ -27,7 +27,7 @@ from g_plots import get_bar_chart, sos_analysis_and_plot
 # G-TENSOR CALCULATION
 g_calculation = 1
 ras_input = '../\
-RASCI_results/cu_mnt2_2-/cu_mnt2_2-_def2tzvp_19_11.out'  # str(sys.argv[1])'''
+RASCI_results/mncn5no_2-/mncn5no_2-_def2tzvp_9_10_d5_sextet_enerproc.out'  # str(sys.argv[1])'''
 
 selected_states = 1 # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
 states_ras = [1,2,3] # States to be included when "selected_states = 0"
@@ -35,10 +35,10 @@ symmetry_selection = 'Ag' # Symmetry selected states
 selected_SOC = 0 # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
 
 # EXCITED STATES ANALYSIS IN ras
-excited_states_analysis = 0
+excited_states_analysis = 1
 new_active_space = 0
 sos_analysis = 0
-bar_plots = 1
+bar_plots = 0
 
 # eom ANALYSIS AND ras-eom ENERGIES EXCHANGE
 eom_information = 0
