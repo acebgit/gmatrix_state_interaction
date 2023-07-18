b3e0fded34cee9701bfcb7fef0e56c0355850b49
acebreiro <antonio.cebreiro@dipc.org>
2023-07-18 19:10:08 +0200

    Generalizing excited states program.

--- test_general.py
+++ test_general.py
@@ -2,26 +2,31 @@
 #          MODULES SELECTION
 #####################################
 import sys
+import os
+import numpy as np
+from tabulate import tabulate
 
 from parser_init import get_eigenenergies, get_selected_states, get_number_of_states, get_spin_orbit_couplings
-
 from parser_gtensor import from_energies_soc_to_g_values, print_g_calculation
-
 from parser_plots import sos_analysis_and_plot
+from parser_excitstates import get_excited_states_analysis
 
 #####################################
 #            INPUT VALUES
 #####################################
-# G-TENSOR CALCULATION
 g_calculation = 1
 ras_input = '\
-roberto_molecules/C22H12B2_8_8.out'
+roberto_molecules/C54H24B4_6_6.out'
+
+several_molecules = 0
+path = "roberto_molecules"
 
 selected_states = 1  # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
 states_ras = [1, 2, 3, 4, 5]  # States to be included when "selected_states = 0"
 symmetry_selection = 'A2'  # Symmetry selected states
 soc_options = 0  # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
 
+excited_states_analysis = 1
 sos_analysis = 1
 
 # OUTPUT
@@ -35,30 +40,31 @@ if write_ras_input == 1:
 ###########################################
 # Import Module
 # https://www.geeksforgeeks.org/how-to-read-multiple-text-files-from-folder-in-python/
-# import os
-# path = "roberto_molecules"
-# os.chdir(path)  # Change the directory
-#
-# g_list = []
-# # iterate through all file
-# for file in os.listdir():
-#     qchem_file = f"{file}"
-#
-#     totalstates = get_number_of_states(qchem_file)
-#     states_ras = get_selected_states(qchem_file, totalstates, states_ras, selected_states, symmetry_selection)
-#     eigenenergies_ras, excitation_energies_ras = get_eigenenergies(qchem_file, totalstates, states_ras)
-#     selected_socs, sz_list, sz_ground = get_spin_orbit_couplings(qchem_file, totalstates, states_ras, soc_options)
-#     g_shift = from_energies_soc_to_g_values(qchem_file, states_ras,
-#                                             totalstates, excitation_energies_ras,
-#                                             selected_socs, sz_list, sz_ground)
-#
-#     qchem_file = qchem_file.replace('.out', '')
-#     g_list.append([qchem_file, np.round(g_shift.real[0]*1000, 3), np.round(g_shift.real[1]*1000, 3),
-#           np.round(g_shift.real[2]*1000, 3)])
-#
-# sorted_list = sorted(g_list)
-# print (tabulate(sorted_list, headers=["molecule", "gxx", "gyy", "gzz"]))
-# exit()
+if several_molecules == 1:
+    os.chdir(path)  # Change the directory
+
+    g_list = []
+    # iterate through all file
+    for file in os.listdir():
+        qchem_file = f"{file}"
+
+        totalstates = get_number_of_states(qchem_file)
+        states_ras = get_selected_states(qchem_file, totalstates, states_ras, selected_states, symmetry_selection)
+        eigenenergies_ras, excitation_energies_ras = get_eigenenergies(qchem_file, totalstates, states_ras)
+        selected_socs, sz_list, sz_ground = get_spin_orbit_couplings(qchem_file, totalstates, states_ras, soc_options, bolvin=0)
+        g_shift = from_energies_soc_to_g_values(qchem_file, states_ras,
+                                                totalstates, excitation_energies_ras,
+                                                selected_socs, sz_list, sz_ground)
+
+        qchem_file = qchem_file.replace('.out', '')
+        g_list.append([qchem_file, np.round(g_shift.real[0]*1000, 3), np.round(g_shift.real[1]*1000, 3),
+              np.round(g_shift.real[2]*1000, 3)])
+
+    sorted_list = sorted(g_list)
+    # for i in range(0, len(sorted_list)):
+    #     print(sorted_list[i, :])
+    print(tabulate(sorted_list, headers=["molecule", "gxx", "gyy", "gzz"]))
+    exit()
 
 #####################################
 #      G-VALUE CALCULATION
@@ -83,5 +89,8 @@ if g_calculation == 1:
 #####################################
 #        PLOT ANALYSIS
 #####################################
+if excited_states_analysis == 1:
+    get_excited_states_analysis(ras_input, cutoff=0.9)
+
 if sos_analysis == 1:
     sos_analysis_and_plot(ras_input)
