99cbf45eb3bb2ed6e4c2ec8601434f83fdffb748
acebreiro <antonio.cebreiro@dipc.org>
2023-09-01 12:12:02 +0200

    Including Roberto molecules outputs

--- test_excit_states.py
+++ test_excit_states.py
@@ -2,6 +2,6 @@
 from parser_excitstates import get_excited_states_analysis
 
 ras_input = '\
-doublets_molecules/mncn5no_2-/mn_prueba.out'
+triplets_molecules/C64H28B4_4_4_concat.out'
 
 get_excited_states_analysis(ras_input, cutoff=0.9)
