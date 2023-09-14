36cc1362a65581f9dcbcfabbb02651d30dbcd01a
acebreiro <antonio.cebreiro@dipc.org>
2023-09-14 13:15:36 +0200

    Using excited states to calculate g-tensor and plotting values

--- test_general.py
+++ test_general.py
@@ -1,10 +1,6 @@
 #####################################
 #          MODULES SELECTION
 #####################################
-import os
-from tabulate import tabulate
-import sys
-
 from parser_gtensor import *
 from parser_excitstates import *
 from parser_plots import *
@@ -17,18 +13,18 @@ from parser_plots import *
 #####################################
 #            INPUT VALUES
 #####################################
-g_calculation = 0
-excited_states_analysis = 1
+g_calculation = 1
+excited_states_analysis = 0
 sos_analysis = 0
-gfactor_excited_states = 1
+gfactor_excited_states = 0
 
 ras_input = '\
 roberto_molecules/C74H32B4/C74H32B4_6_6_singletref_concat_triplets.out'
 several_molecules = 0
 path = "roberto_molecules"
 
-selected_states = 1  # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
-states_ras = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]  # States to be included when "selected_states = 0"
+selected_states = 0  # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
+states_ras = [16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20]  # States to be included when "selected_states = 0"
 # [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 symmetry_selection = 'A2'  # Symmetry selected states
 soc_options = 0  # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
