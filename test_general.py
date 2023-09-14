ba155ef4bcce2dfdc654fc6fefc3de7286c1a5d7
acebreiro <antonio.cebreiro@dipc.org>
2023-09-14 17:43:23 +0200

    Roberto molecules inputs

--- test_general.py
+++ test_general.py
@@ -13,10 +13,10 @@ from parser_plots import *
 #####################################
 #            INPUT VALUES
 #####################################
-g_calculation = 1
+g_calculation = 0
 excited_states_analysis = 0
 sos_analysis = 0
-gfactor_excited_states = 0
+gfactor_excited_states = 1
 
 ras_input = '\
 roberto_molecules/C74H32B4/C74H32B4_6_6_singletref_concat_triplets.out'
@@ -24,7 +24,7 @@ several_molecules = 0
 path = "roberto_molecules"
 
 selected_states = 0  # 0: use "state_ras" ; 1: use all states ; 2: use states by selected symmetry
-states_ras = [16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20]  # States to be included when "selected_states = 0"
+states_ras = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]  # States to be included when "selected_states = 0"
 # [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 symmetry_selection = 'A2'  # Symmetry selected states
 soc_options = 0  # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
