954a70625c8e4a1f0012ca44b748758e21f1dca5
acebreiro <antonio.cebreiro@dipc.org>
2023-10-18 15:08:21 +0200

    Calculating g-tensor of diatomic molecules

--- test_general.py
+++ test_general.py
@@ -13,16 +13,16 @@ from parser_plots import sos_analysis_and_plot, gfactor_all_states
 #            INPUT VALUES
 #####################################
 ras_input = '\
-triplets_molecules/s2_10_9_triplets.ou'
+triplets_molecules/s2_10_9_triplets.out'
 
 g_calculation = 1
-excited_states_analysis = 1
-sos_analysis = 1
+excited_states_analysis = 0
+sos_analysis = 0
 gfactor_excited_states = 0
 ppm = 0
 
-state_selection = 1 # 0: use "state_ras" ; 1: use all states_selected ; 2: use states_selected by selected symmetry
-states_ras = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]  # States to be included when "states_option = 0"
+state_selection = 0 # 0: use "state_ras" ; 1: use all states_selected ; 2: use states_selected by selected symmetry
+states_ras = [1,5,6]  # States to be included when "states_option = 0"
 # [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 symmetry_selection = 'A2'  # Symmetry selected states_selected
 soc_options = 0  # 0: Total mean-field SOC matrix; 1: 1-elec SOC matrix; 2: 2-elec mean-field SOC matrix
